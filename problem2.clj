2b1431b42fa9c9fa0b01f761109778e9421d4f15
boxp <tiyotiyouda@gmail.com>
2013-04-06 21:57:47 +0900

    試行錯誤

--- problem2.clj
+++ problem2.clj
@@ -1,15 +1,13 @@
 (loop [result #{}]
-  (if (> (last result) 4000000) 
-    (reduce + 
-      (filter 
-        (even? (drop-last result)) 
-        (drop-last result))))
   (if (= result #{}) 
     (conj result 1 2)
-    (recur 
-      (conj 
-        result 
-        (reduce + 
-          (drop 
-            (- (count result) 2)) 
-            result)))))
+    (do (if (> (last result) 4000000) 
+          (reduce + 
+            (filter 
+              even?  
+              (drop-last result))))
+        (recur (conj result 
+          (reduce + 
+            (drop 
+              (- (count result) 2)) 
+              result))))))
